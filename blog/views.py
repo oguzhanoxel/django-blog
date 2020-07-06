24618633c211d8b40be7cdf27fe73fe130a8e597
oguzhanoxel <oguzhanoksel@hotmail.com>
2020-07-06 22:49:04 +0300

    View'a post_delete eklendi , Paginator eklendi, static dosyası için bazı değişiklikler yapıldı

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect, Http404
 from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+    page = request.GET.get('page',1)
+    paginator = Paginator(posts, 5)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -45,3 +56,12 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+
+    if not request.user.is_authenticated:
+        raise Http404()
+
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
